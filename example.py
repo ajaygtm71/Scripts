9bb0a11cfdca6d40ac343ce3be5911abeb4364b0
himel <himelmahmud44@gmail.com>
2020-12-26 08:49:36 +0600

    Added Python example and wirte code

---
+++ example.py
@@ -0,0 +1,12 @@
+"""Question 1: Given a two integer numbers return their product and  if the product
+is greater than 1000, then return their sum
+"""
+def multi_or_sum(num1,num2):
+    product = num1*num2
+    if product > 1000:
+        return num1+num2
+    else:
+        return product
+
+result = multi_or_sum(40,30)
+print("The result is:",result)
